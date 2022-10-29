a5daf3e56d376495fdae9ff67dc9b2075e9f2b60
Cuadra13 <nelsoncuadra844@gmail.com>
2022-10-28 18:53:16 -0600

    Update index.js

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,8 +1,12 @@
 const http = require('http')
+const host = "127.0.0.1"
+const port = 80;
 const server = http.createServer(function(peticion, respuesta){
     respuesta.writeHead(200,{'Content-Type':'application.txt'})
     respuesta.write('Tablas')
     respuesta.end()
 });
 
-server.listen()
+server.listen(8080, 'localhost', function(error){
+    console.log('Servidor disponible:  htttp://${host}:${port}')
+})
