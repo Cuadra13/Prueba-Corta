a7ca539c5f29b8cd845db6e6c65ff7ec34a52b38
Cuadra13 <nelsoncuadra844@gmail.com>
2022-10-28 19:06:00 -0600

    Update http.js

--- src/Routes/http.js
+++ src/Routes/http.js
@@ -3,3 +3,11 @@ const router = Router();
 const fs = require('fs');
 const json_books = fs.readFileSync('src/books.json ', 'utf-8');
 let books = JSON.parse(json_books);
+
+router.get('/', (req,res) => {
+    res.render('index.ejs', {
+        books
+    });
+});
+
+router.get()
